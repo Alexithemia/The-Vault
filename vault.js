715b91634ad0d14a092b1a4d86b9aefc492d9a63
Tyler <alexithemia@gmail.com>
2019-01-02 17:29:03 -1000

    complete

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  let newObj = {};
+
+  function getValue(key) {
+    if (newObj[key] === undefined) {
+      return null;
+    } else {
+      return newObj[key];
+    }
+  }
+
+  function setValue(key, value) {
+    newObj[key] = value;
+  }
+
+  return {
+    setValue, getValue
+  };
 };
